4e01942641c41ce8d95be10a7abb350aff7cdb7e
Theo Cardozo <theocardozo55@gmail.com>
2025-07-06 11:28:09 -0300

    Configuracao inicial do Spring Boot

---
+++ src/main/java/br/com/teste_full_stack/TesteFullStackApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste_full_stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teste_full_stack/TesteFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste_full_stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
